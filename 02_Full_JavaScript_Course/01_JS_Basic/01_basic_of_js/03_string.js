45799012847abfdece28db07d2be0a7381a15b72
Sonak <142824400+sonak1029@users.noreply.github.com>
2025-04-12 16:06:56 +0000

    paste file

--- 02_Full_JavaScript_Course/01_JS_Basic/01_basic_of_js/03_string.js
+++ 02_Full_JavaScript_Course/01_JS_Basic/01_basic_of_js/03_string.js
@@ -3,4 +3,9 @@ const repoCount = 50;
 
 // console.log(name + " " + repoCount + " Value");
 
-console.log(`Hello And welcome to the ${name} Community and my total repo is ${repoCount}`);
+// console.log(`Hello And welcome to the ${name} Community and my total repo is ${repoCount}`);
+
+const gameName = new String("Sonak");
+
+// console.log(gameName);
+console.log(gameName[0]);
