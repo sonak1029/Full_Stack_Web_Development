50e5af7e9611d5537a57e2ce06db624973342d70
Sonak <142824400+sonak1029@users.noreply.github.com>
2025-04-15 16:31:51 +0000

    string practice

--- 02_Full_JavaScript_Course/01_JS_Basic/01_basic_of_js/03_string.js
+++ 02_Full_JavaScript_Course/01_JS_Basic/01_basic_of_js/03_string.js
@@ -5,7 +5,45 @@ const repoCount = 50;
 
 // console.log(`Hello And welcome to the ${name} Community and my total repo is ${repoCount}`);
 
-const gameName = new String("Sonak");
+const gameName = new String("pro-gra-mmi-ng");
 
 // console.log(gameName);
 console.log(gameName[0]);
+
+// This all are the methods
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// here we check kaise index par kon sa value h
+// console.log(gameName.charAt(4));
+
+// Returns the index of the first occurrence of the specified value in the string/array. Returns -1 if not found.
+// console.log(gameName.indexOf('m'));
+
+
+const newString = gameName.substring(-8, 6);
+// console.log(newString);
+
+// That is the working part of slice
+/*
+    C  a  l  l  O  f  D  u  t  y
+    0  1  2  3  4  5  6  7  8  9
+    -10 -9 -8 -7 -6 -5 -4 -3 -2 -1
+*/
+
+
+const anotherString = gameName.slice(-8, 5);
+// console.log(anotherString);
+
+// this one is only work when user give lot's of white space
+const str = "       CallOfDuty       ";
+// console.log(str);
+// console.log(str.trim());
+
+
+const url = "https://www.youtube.com/watch?v=fozwNnFunlo&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=11";
+
+// console.log(url.replace('=', '-'));
+// console.log(url.includes('sonak'));
+
+console.log(gameName.split('-'));
