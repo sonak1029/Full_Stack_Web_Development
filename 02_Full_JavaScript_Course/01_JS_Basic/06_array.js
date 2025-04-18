c7a0623b93c284ef8832ac617183978f5ebe8192
Sonak <142824400+sonak1029@users.noreply.github.com>
2025-04-18 16:41:09 +0000

    practice array

--- 02_Full_JavaScript_Course/01_JS_Basic/06_array.js
+++ 02_Full_JavaScript_Course/01_JS_Basic/06_array.js
@@ -6,8 +6,31 @@ let num = [10, 20, 30, 40, 50];
 let myArr = new Array(1, 2, 3, 4, 5);
 // console.log(`${myArr}`);
 
-console.log(str);
+// console.log(str);
 // console.log(`${str}`);
 
 num.push(60);
+// console.log(num);
+
+// Here we use some javascript methodes
+num.pop();
+// console.log(num); // it will pop the element
+
+num.unshift(60);
 console.log(num);
+
+num.shift();
+// console.log(num);
+
+// if you want only ture/false ans then we can use this
+// console.log(num.includes(40));
+// console.log(num.indexOf(40));
+
+// const arr = num.join();
+// console.log(arr);
+// console.log(typeof arr);
+
+
+// slice and splice
+const a = str.slice(1, 4);
+console.log(a);
