08bdacfa617718e3856c3e57439dab2d6297fb8c
Sonak <142824400+sonak1029@users.noreply.github.com>
2025-04-19 16:19:51 +0000

    array methodes

--- 02_Full_JavaScript_Course/01_JS_Basic/06_array.js
+++ 02_Full_JavaScript_Course/01_JS_Basic/06_array.js
@@ -17,7 +17,7 @@ num.pop();
 // console.log(num); // it will pop the element
 
 num.unshift(60);
-console.log(num);
+// console.log(num);
 
 num.shift();
 // console.log(num);
@@ -32,5 +32,67 @@ num.shift();
 
 
 // slice and splice
-const a = str.slice(1, 4);
-console.log(a);
+// const a = str.slice(1, 4);
+// console.log(a);
+
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// console.log(fruits);
+
+
+// const c = fruits.slice(0, 3);
+// console.log(c);
+// console.log(fruits);
+
+
+// console.log("\n");
+
+
+// const d = fruits.splice(3);
+// console.log(d);
+// console.log(fruits);
+
+
+
+// Array Part 2
+const marvel_hero = ['ironman', 'thor', 'spiderman'];
+const dc_hero = ['superman', 'flash', 'batman'];
+
+// marvel_hero.push(dc_hero);
+// console.log(marvel_hero);
+// console.log(marvel_hero[3][0]);
+
+// Here we use concat
+// const all_hero = marvel_hero.concat(dc_hero);
+// console.log(all_hero);
+
+
+// Here we use spread // ek glass the usko mene drop kiya to wo kya ho gaya spread ho gaya
+// const all_new_hero = [...marvel_hero, ...dc_hero];
+// console.log(all_new_hero);
+
+// Here we use flat methode if we have array ka ander  array use ander ek or array then ek or array usek ander bhe array 
+// ab agar mujha ek he single array me ans chiya to me ya use karung flat methode
+const new_array = [1, 2, 3, [4, 5, 6], 7, [8, 9, 10], 11, [12, 13, 14, [15, 16]]];
+// console.log(new_array);
+const real_another_array = new_array.flat(5); // it will give the value in one single arrray 
+// const real_another_array = new_array.flat(Infinity);
+// console.log(real_another_array);
+
+
+// ya jo methode h suppose jab bhe ham khai sa data to late h to khabi khabi hamea kuch alag 
+// form me data dakhna ko milta h ab agar data hamea array ke from me chiya to ya karn hoga
+// console.log(Array.isArray('sonak'));
+// console.log(Array.from('sonak'));
+// console.log(Array.from({ // that is very intersting case for interview
+//     name: 'Sonak',
+//     age: 19,
+//     phone_Number: 12345678,
+//     dob: '10/08/2021',
+// }));
+
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3)); // ya bhe ek tarika h array ka ander value ko insert karna ka
